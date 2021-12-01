6b4aef3f6dc3d0b6bec10c780d8bb47c6c120cb1
biandratti <maximiliano.biandratti@iohk.io>
2021-12-01 17:01:36 +0100

    adding mocked classes

--- src/component/nomad_event_test.go
+++ src/component/nomad_event_test.go
@@ -5,58 +5,14 @@ import (
 	"errors"
 	"fmt"
 	nomad "github.com/hashicorp/nomad/api"
-	"github.com/input-output-hk/cicero/src/application"
-	"github.com/jackc/pgx/v4"
+	"github.com/input-output-hk/cicero/src/application/mocks"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"log"
 	"os"
 	"testing"
 )
 
-type NomadEventServiceMocked struct {
-	mock.Mock
-}
-
-func (n *NomadEventServiceMocked) Save(tx pgx.Tx, event *nomad.Event) error {
-	panic("implement me")
-}
-
-func (n *NomadEventServiceMocked) GetLastNomadEvent() (event uint64, err error) {
-	args := n.Called()
-	if args.Get(0) != nil {
-		event = args.Get(0).(uint64)
-	}
-	err = args.Error(1)
-	return
-}
-
-func (n *NomadEventServiceMocked) GetEventAllocByWorkflowId(u uint64) (map[string]application.AllocWrapper, error) {
-	panic("implement me")
-}
-
-type NomadClientMocked struct {
-	mock.Mock
-}
-
-func (n *NomadClientMocked) EventStream(ctx context.Context, index uint64) (stream <-chan *nomad.Events, err error) {
-	args := n.Called(ctx, index)
-	if args.Get(0) != nil {
-		stream = make(chan *nomad.Events, 0)
-	}
-	err = args.Error(1)
-	return
-}
-
-func (n *NomadClientMocked) JobsRegister(job *nomad.Job, q *nomad.WriteOptions) (*nomad.JobRegisterResponse, *nomad.WriteMeta, error) {
-	panic("implement me")
-}
-
-func (n *NomadClientMocked) JobsDeregister(jobID string, purge bool, q *nomad.WriteOptions) (string, *nomad.WriteMeta, error) {
-	panic("implement me")
-}
-
-func buildNomadEventConsumerMocked(nomadEventServiceMocked *NomadEventServiceMocked, nomadClientMocked *NomadClientMocked) *NomadEventConsumer {
+func buildNomadEventConsumerMocked(nomadEventServiceMocked *mocks.NomadEventService, nomadClientMocked *mocks.NomadClient) *NomadEventConsumer {
 	return &NomadEventConsumer{
 		Logger:            log.New(os.Stderr, "NomadEventConsumerTest: ", log.LstdFlags),
 		NomadEventService: nomadEventServiceMocked,
@@ -70,9 +26,9 @@ func TestStartWorkflowFailureToListenNomadEvent(t *testing.T) {
 	//given
 	eventId := uint64(1)
 	ctx := context.Background()
-	stream := make(chan *nomad.Events, 1)
-	nomadEventService := new(NomadEventServiceMocked)
-	nomadClient := new(NomadClientMocked)
+	stream := make(<-chan *nomad.Events, 0)
+	nomadEventService := &mocks.NomadEventService{}
+	nomadClient := &mocks.NomadClient{}
 	nomadEventConsumer := buildNomadEventConsumerMocked(nomadEventService, nomadClient)
 	nomadEventService.On("GetLastNomadEvent").Return(eventId, nil)
 	errorMessage := "Some error"
