c14009dc59c0afc4762dc2ea10a9f488d7936f68
Robin Stumm <robin.stumm@iohk.io>
2021-12-01 20:02:10 +0100

    make script runners not conflict with each other if multiple are used in a task

--- runners.nix
+++ runners.nix
@@ -1,34 +1,34 @@
 final: prev:
 
 let
-  drvName = "workflow-action-script";
+  commonName = "workflow-action-script";
 
   runner = drv:
-    prev.writers.writeBashBin drvName ''
+    prev.writers.writeBashBin "${drv.name}-runner" ''
       set -exuo pipefail
 
       export PATH="$PATH:${
         prev.lib.makeBinPath
-        (with final; [ coreutils gnutar xz liftbridge-cli ])
+        (with final; [ coreutils gnutar xz ])
       }"
       export CICERO_SCRIPT=$(< "$1")
       exec ${drv}
     '';
 in {
-  run-bash = runner (prev.writers.writeBash drvName ''
+  run-bash = runner (prev.writers.writeBash "${commonName}-bash" ''
     eval "$CICERO_SCRIPT"
   '');
 
-  run-python = runner (prev.writers.writePython3 drvName { } ''
+  run-python = runner (prev.writers.writePython3 "${commonName}-python" { } ''
     import os
     eval(os.environ['CICERO_SCRIPT'])
   '');
 
-  run-perl = runner (prev.writers.writePerl drvName { } ''
+  run-perl = runner (prev.writers.writePerl "${commonName}-perl" { } ''
     eval $ENV{'CICERO_SCRIPT'};
   '');
 
-  run-js = runner (prev.writers.writeJS drvName { } ''
+  run-js = runner (prev.writers.writeJS "${commonName}-js" { } ''
     const process = require('process')
     eval(process.env.CICERO_SCRIPT)
   '');
