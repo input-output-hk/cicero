e4d021654e5008cfaf41e0e92369c1c4550d12e5
Robin Stumm <robin.stumm@iohk.io>
2021-12-31 11:10:52 +0100

    simplify example

--- actions/examples/behaviors/onInputChange.nix
+++ actions/examples/behaviors/onInputChange.nix
@@ -3,11 +3,11 @@
 std.behavior.onInputChange "run" name args {
   inputs.run = ''
     // arbitrary value that identifies this run
-    "${name}/run": _
+    "${name}": _
   '';
 
   job = { run }:
     actionLib.simpleJob args (std.script "bash" ''
-      echo 'Running once because I have not run with "'${lib.escapeShellArg run.value."${name}/run"}'" yet.'
+      echo 'Running once because I have not run with "'${lib.escapeShellArg run.value.${name}}'" yet.'
     '');
 }
