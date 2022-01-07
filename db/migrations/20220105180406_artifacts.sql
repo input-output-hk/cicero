422c9880a881e4ffbed32847fb97c7e995b0c5dc
Robin Stumm <robin.stumm@iohk.io>
2022-01-07 12:13:07 +0100

    use large object extension for fact binaries for good measure
    not really required because a fact's binary never changes

--- db/migrations/20220105180406_artifacts.sql
+++ db/migrations/20220105180406_artifacts.sql
@@ -5,7 +5,12 @@ CREATE EXTENSION IF NOT EXISTS lo;
 ALTER TABLE facts
 ADD COLUMN "binary" lo CHECK (("binary" IS NULL) = (binary_hash IS NULL));
 
+CREATE TRIGGER "binary" BEFORE UPDATE OR DELETE ON facts
+FOR EACH ROW EXECUTE FUNCTION lo_manage("binary");
+
 -- migrate:down
 
 ALTER TABLE facts
 DROP COLUMN "binary";
+
+DROP TRIGGER "binary" ON facts;
