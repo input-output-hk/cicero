3bd2e09b895c22b8b706430c3c9882efa06a9b58
Robin Stumm <robin.stumm@iohk.io>
2022-01-12 13:53:31 +0100

    [WIP] add schemathesis test to "cicero/ci" action

--- actions/cicero/ci.nix
+++ actions/cicero/ci.nix
@@ -14,26 +14,63 @@ std.behavior.onInputChange "start" name args
   job = { start }:
     let cfg = start.value.${name}.start; in
     std.chain args [
-      actionLib.simpleJob
+      actionLib.jobDefaults
 
-      (lib.optionalAttrs (cfg ? statuses_url)
-        (std.github.reportStatus cfg.statuses_url))
-
-      (std.git.clone cfg)
+      (std.escapeNames [ ] [ ])
 
       {
-        resources = {
-          memory = 4 * 1024;
-          cpu = 16000;
+        cicero-ci.group.cicero = {
+          network = {
+            mode = "bridge";
+            port.http.to = 8080;
+          };
+
+          task = {
+            lintAndBuild = std.chain args [
+              (lib.optionalAttrs (cfg ? statuses_url)
+                (std.github.reportStatus cfg.statuses_url))
+
+              (std.git.clone cfg)
+
+              {
+                resources = {
+                  memory = 4 * 1024;
+                  cpu = 16000;
+                };
+              }
+
+              std.nix.develop
+              (std.wrapScript "bash" (next: ''
+                lint
+                ${lib.escapeShellArg next}
+              ''))
+
+              std.nix.build
+            ];
+
+            cicero = {
+              config.nixos = "github:input-output-hk/cicero/${cfg.sha}#dev";
+
+              lifecycle = {
+                hook = "prestart";
+                sidecar = true;
+              };
+            };
+
+            schemathesis = std.chain args [
+              (lib.optionalAttrs (cfg ? statuses_url)
+                (std.github.reportStatus cfg.statuses_url))
+
+              (std.git.clone cfg)
+
+              std.nix.develop
+
+              (std.script "bash" ''
+                schemathesis run http://localhost:$NOMAD_PORT_http/documentation/cicero.yaml --validate-schema=false
+              '')
+            ];
+          };
         };
       }
-
-      std.nix.develop
-      (std.wrapScript "bash" (next: ''
-        lint
-        ${lib.escapeShellArg next}
-      ''))
-
-      std.nix.build
     ];
 }
