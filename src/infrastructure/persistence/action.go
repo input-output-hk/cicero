input-output-hk/cicero

commit 2006a4e3df841e212cb0b4c454932fd0d21c7dc5

use singular for db table names
merge facts and artifacts migrations

src/infrastructure/persistence/action.go
@@ -24,7 +24,7 @@ func NewActionRepository(db config.PgxIface) repository.ActionRepository {
 func (a *actionRepository) GetById(id uuid.UUID) (action domain.Action, err error) {
 	err = pgxscan.Get(
 		context.Background(), a.DB, &action,
-		`SELECT * FROM actions WHERE id = $1`,
+		`SELECT * FROM action WHERE id = $1`,
 		id,
 	)
 	return
@@ -33,10 +33,10 @@ func (a *actionRepository) GetById(id uuid.UUID) (action domain.Action, err erro
 func (a *actionRepository) GetByRunId(id uuid.UUID) (action domain.Action, err error) {
 	err = pgxscan.Get(
 		context.Background(), a.DB, &action,
-		`SELECT * FROM actions WHERE EXISTS (
-			SELECT NULL FROM runs WHERE
-				runs.nomad_job_id = $1 AND
-				runs.action_id = actions.id
+		`SELECT * FROM action WHERE EXISTS (
+			SELECT NULL FROM run WHERE
+				run.nomad_job_id = $1 AND
+				run.action_id = actions.id
 		)`,
 		id,
 	)
@@ -46,7 +46,7 @@ func (a *actionRepository) GetByRunId(id uuid.UUID) (action domain.Action, err e
 func (a *actionRepository) GetLatestByName(name string) (action domain.Action, err error) {
 	err = pgxscan.Get(
 		context.Background(), a.DB, &action,
-		`SELECT DISTINCT ON (name) * FROM actions WHERE name = $1 ORDER BY name, created_at DESC`,
+		`SELECT DISTINCT ON (name) * FROM action WHERE name = $1 ORDER BY name, created_at DESC`,
 		name,
 	)
 	return
@@ -55,7 +55,7 @@ func (a *actionRepository) GetLatestByName(name string) (action domain.Action, e
 func (a *actionRepository) GetAll() (actions []*domain.Action, err error) {
 	err = pgxscan.Select(
 		context.Background(), a.DB, &actions,
-		`SELECT * FROM actions ORDER BY created_at DESC`,
+		`SELECT * FROM action ORDER BY created_at DESC`,
 	)
 	return
 }
@@ -66,9 +66,9 @@ func (a *actionRepository) Save(tx pgx.Tx, action *domain.Action) error {
 	} else {
 		var sql string
 		if action.ID == (uuid.UUID{}) {
-			sql = `INSERT INTO actions (    name, source, inputs) VALUES (    $2, $3, $4) RETURNING id, created_at`
+			sql = `INSERT INTO action (    name, source, inputs) VALUES (    $2, $3, $4) RETURNING id, created_at`
 		} else {
-			sql = `INSERT INTO actions (id, name, source, inputs) VALUES ($1, $2, $3, $4) RETURNING id, created_at`
+			sql = `INSERT INTO action (id, name, source, inputs) VALUES ($1, $2, $3, $4) RETURNING id, created_at`
 		}
 		return tx.QueryRow(
 			context.Background(),
@@ -81,7 +81,7 @@ func (a *actionRepository) Save(tx pgx.Tx, action *domain.Action) error {
 func (a *actionRepository) GetCurrent(tx pgx.Tx) (actions []*domain.Action, err error) {
 	err = pgxscan.Select(
 		context.Background(), tx, &actions,
-		`SELECT DISTINCT ON (name) * FROM actions ORDER BY name, created_at DESC`,
+		`SELECT DISTINCT ON (name) * FROM action ORDER BY name, created_at DESC`,
 	)
 	return
 }

db/migrations/20211029212344_create_nomad_events.sql
@@ -1,6 +1,6 @@
 -- migrate:up
 
-create table nomad_events (
+create table nomad_event (
 	topic text NOT NULL,
 	type text NOT NULL,
 	key text NOT NULL,
@@ -11,4 +11,4 @@ create table nomad_events (
 
 -- migrate:down
 
-drop table nomad_events;
+drop table nomad_event;

db/migrations/20211216213705_create_actions.sql
@@ -2,7 +2,7 @@
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE TABLE actions (
+CREATE TABLE action (
 	id uuid PRIMARY KEY DEFAULT public.gen_random_uuid(),
 	name text NOT NULL CHECK (name <> ''),
 	meta jsonb,
@@ -13,4 +13,4 @@ CREATE TABLE actions (
 
 -- migrate:down
 
-DROP TABLE actions;
+DROP TABLE action;

db/migrations/20211216213706_create_runs.sql
@@ -2,21 +2,21 @@
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE TABLE runs (
+CREATE TABLE run (
 	nomad_job_id uuid PRIMARY KEY DEFAULT public.gen_random_uuid(),
 	action_id uuid NOT NULL,
 	created_at timestamp NOT NULL DEFAULT NOW(),
 	finished_at timestamp,
-	FOREIGN KEY (action_id) REFERENCES actions (id)
+	FOREIGN KEY (action_id) REFERENCES action (id)
 );
 
-CREATE TABLE run_outputs (
+CREATE TABLE run_output (
 	run_id uuid PRIMARY KEY,
 	success jsonb,
 	failure jsonb,
-	FOREIGN KEY (run_id) REFERENCES runs (nomad_job_id)
+	FOREIGN KEY (run_id) REFERENCES run (nomad_job_id)
 );
 
 -- migrate:down
 
-DROP TABLE runs, run_outputs;
+DROP TABLE run, run_output;

db/migrations/20211216213707_create_facts.sql
@@ -1,16 +1,21 @@
 -- migrate:up
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS lo;
 
-CREATE TABLE facts (
+CREATE TABLE fact (
 	id uuid PRIMARY KEY DEFAULT public.gen_random_uuid(),
 	run_id uuid,
 	value jsonb NOT NULL,
 	binary_hash text,
+	"binary" lo CHECK (("binary" IS NULL) = (binary_hash IS NULL)),
 	created_at timestamp NOT NULL DEFAULT NOW(),
-	FOREIGN KEY (run_id) REFERENCES runs (nomad_job_id)
+	FOREIGN KEY (run_id) REFERENCES run (nomad_job_id)
 );
+
+CREATE TRIGGER "binary" BEFORE UPDATE OR DELETE ON fact
+FOR EACH ROW EXECUTE FUNCTION lo_manage("binary");
 
 -- migrate:down
 
-DROP TABLE facts;
+DROP TABLE fact;

db/migrations/20220105180406_artifacts.sql
@@ -1,16 +1,0 @@
--- migrate:up
-
-CREATE EXTENSION IF NOT EXISTS lo;
-
-ALTER TABLE facts
-ADD COLUMN "binary" lo CHECK (("binary" IS NULL) = (binary_hash IS NULL));
-
-CREATE TRIGGER "binary" BEFORE UPDATE OR DELETE ON facts
-FOR EACH ROW EXECUTE FUNCTION lo_manage("binary");
-
--- migrate:down
-
-ALTER TABLE facts
-DROP COLUMN "binary";
-
-DROP TRIGGER "binary" ON facts;

db/migrations/20220111101227_postgres_api.sql
@@ -4,21 +4,21 @@ CREATE SCHEMA api;
 
 GRANT USAGE ON SCHEMA api TO cicero_api;
 
-CREATE VIEW api.current_actions AS
+CREATE VIEW api.current_action AS
 SELECT DISTINCT ON (name) *
-FROM public.actions
+FROM public.action
 ORDER BY name, created_at DESC;
 
-CREATE VIEW api.artifacts AS
+CREATE VIEW api.artifact AS
 SELECT *
-FROM facts
+FROM fact
 WHERE "binary" IS NOT NULL;
 
 CREATE FUNCTION api.input_select_all(paths jsonb)
-RETURNS SETOF facts
+RETURNS SETOF fact
 LANGUAGE plpgsql AS $$
 	DECLARE
-		sql text := 'SELECT * FROM public.facts WHERE TRUE';
+		sql text := 'SELECT * FROM public.fact WHERE TRUE';
 		parts text[];
 
 		i int := 1;
@@ -44,7 +44,7 @@ LANGUAGE plpgsql AS $$
 $$;
 
 CREATE FUNCTION api.input_select_latest(paths jsonb)
-RETURNS SETOF facts
+RETURNS SETOF fact
 LANGUAGE plpgsql AS $$
 	BEGIN
 		RETURN QUERY

src/infrastructure/persistence/action_test.go
@@ -76,7 +76,7 @@ func TestShouldSaveAction(t *testing.T) {
 	}
 	mock, tx := mocks.BuildTransaction(context.Background(), t)
 	rows := mock.NewRows([]string{"id", "created_at"}).AddRow(actionId, dateTime)
-	mock.ExpectQuery("INSERT INTO actions").WithArgs(action.ID, action.Name, action.Source, marshalInputs).WillReturnRows(rows)
+	mock.ExpectQuery("INSERT INTO action").WithArgs(action.ID, action.Name, action.Source, marshalInputs).WillReturnRows(rows)
 	mock.ExpectCommit()
 	repository := NewActionRepository(mock)
 

commit a5310d1ea4c7cceaf37bfb8cdcc60d3bdba1e102

fix SQL query
broken by 2006a4e

src/infrastructure/persistence/action.go
@@ -36,7 +36,7 @@ func (a *actionRepository) GetByRunId(id uuid.UUID) (action domain.Action, err e
 		`SELECT * FROM action WHERE EXISTS (
 			SELECT NULL FROM run WHERE
 				run.nomad_job_id = $1 AND
-				run.action_id = actions.id
+				run.action_id = action.id
 		)`,
 		id,
 	)