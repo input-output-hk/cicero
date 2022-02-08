c4525b153bc909501b6e1a3ce239b823b8c6d6d0
Robin Stumm <robin.stumm@iohk.io>
2022-02-08 10:39:02 +0100

    add general PR CI action

---
+++ actions/std/pr.nix
@@ -0,0 +1,31 @@
+{ ... }:
+
+{
+  inputs.github-event = ''
+    "github-event": {
+      repository: {
+        name: string
+        owner: login: string
+        full_name?: "\(owner.login)/\(name)"
+      }
+      action: "opened" | "reopened" | "synchronize"
+      pull_request: {
+        "_links": statuses: href: string
+        head: {
+          sha: string
+          repo: clone_url: string
+        }
+      }
+    }
+  '';
+
+  output = { github-event }:
+    let inherit (github-event.value.github-event) pull_request repository; in
+    {
+      success."${repository.name}/ci".start = {
+        inherit (pull_request.head.repo) clone_url;
+        inherit (pull_request.head) sha;
+        statuses_url = pull_request._links.statuses.href;
+      };
+    };
+}
