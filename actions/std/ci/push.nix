3d0772ab93a5245734added74706a59ef0c82b5f
Robin Stumm <robin.stumm@iohk.io>
2022-02-11 17:18:26 +0100

    add std/ci/push action

---
+++ actions/std/ci/push.nix
@@ -0,0 +1,31 @@
+{ ... }:
+
+{
+  inputs.github-event = ''
+    "github-event": {
+      pusher: {}
+      deleted: false
+      ref: "refs/heads/\(repository.default_branch)"
+      repository: {
+        name: string
+        owner: login: string
+        default_branch: string
+        clone_url: string
+      }
+      head_commit: {
+        id: string
+        distinct: true
+      }
+    }
+  '';
+
+  output = { github-event }:
+    let inherit (github-event.value.github-event) repository head_commit; in
+    {
+      success."${repository.name}/ci".start = {
+        inherit (repository) clone_url;
+        sha = head_commit.id;
+        statuses_url = "https://api.github.com/repos/${repository.owner.login}/${repository.name}/statuses/${head_commit.id}";
+      };
+    };
+}
