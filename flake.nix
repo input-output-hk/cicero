6ca85c938db2f4f638864b67bd8b6fd12d49e08b
Robert Schardt <robert.schardt@iohk.io>
2022-02-11 17:23:44 +0100

    Update nix-cache-proxy cmd

--- flake.nix
+++ flake.nix
@@ -153,7 +153,7 @@
 
                 nix key generate-secret --key-name foo > skey
 
-                ${nix-cache-proxy}/bin/nix-cache-proxy \
+                ${nix-cache-proxy.defaultPackage.${system}}/bin/nix-cache-proxy \
                 --substituters "https://cache.nixos.org" "https://hydra.iohk.io" \
                 --secret-key-files ./skey \
                 --trusted-public-keys "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" \
