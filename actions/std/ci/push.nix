b8e2a4ce9c523bc5d7149d13ac11a78192180c8f
Robin Stumm <robin.stumm@iohk.io>
2022-03-04 18:52:06 +0100

    actions/std/ci/push: output branch info

--- actions/std/ci/push.nix
+++ actions/std/ci/push.nix
@@ -18,10 +18,11 @@
   '';
 
   output = {github-event}: let
-    inherit (github-event.value.github-event) repository head_commit;
+    inherit (github-event.value.github-event) ref repository head_commit;
   in {
     success."${repository.name}/ci".start = {
-      inherit (repository) clone_url;
+      inherit (repository) clone_url default_branch;
+      inherit ref;
       sha = head_commit.id;
       statuses_url = "https://api.github.com/repos/${repository.owner.login}/${repository.name}/statuses/${head_commit.id}";
     };
