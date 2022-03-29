37a0e0b9cb55af732ff64a0ee0c55960ce3ccf67
Michael Fellinger <michael.fellinger@iohk.io>
2022-03-29 13:33:35 +0200

    fix entrypoint darkhttpd

--- pkgs/handbook/entrypoint.nix
+++ pkgs/handbook/entrypoint.nix
@@ -4,5 +4,5 @@
   handbook,
 }:
 writeShellScriptBin "serve-cicero-handbook" ''
-  exec darkhttpd ${handbook} --port "''${NOMAD_PORT_http:-8080}"
+  exec ${darkhttpd}/bin/darkhttpd ${handbook} --port "''${NOMAD_PORT_http:-8080}"
 ''
