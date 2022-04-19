1911e542788cb394e728491be10749e22a05cfcf
Robin Stumm <robin.stumm@iohk.io>
2022-04-19 15:30:45 +0200

    less magic numbers

--- src/domain/types.go
+++ src/domain/types.go
@@ -200,8 +200,8 @@ type MD5Sum [16]byte
 func (self *MD5Sum) Scan(value interface{}) error {
 	if b, ok := value.([]byte); !ok {
 		return fmt.Errorf("Cannot scan %T into MD5Sum", value)
-	} else if copied := copy(self[:], b); copied != 16 {
-		return fmt.Errorf("Could only copy %d/16 bytes into MD5Sum", copied)
+	} else if copied := copy(self[:], b); copied != len(*self) {
+		return fmt.Errorf("Could only copy %d/%d bytes into MD5Sum", copied, len(*self))
 	}
 	return nil
 }
