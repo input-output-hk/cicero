100c954ff8c0969b2c0de7a0dcdc68efe1e0dd26
Robin Stumm <robin.stumm@iohk.io>
2022-04-28 14:04:23 +0200

    nix evaluator: push tullia images

--- pkgs/cicero/evaluators/nix/cicero-evaluator-nix.sh
+++ pkgs/cicero/evaluators/nix/cicero-evaluator-nix.sh
@@ -14,6 +14,7 @@ function usage {
 		echo
 		echo 'The following env vars are optional:'
 		echo -e '\t- CICERO_EVALUATOR_NIX_STACKTRACE'
+		echo -e '\t- CICERO_EVALUATOR_NIX_TULLIA (set to 0 to disable Tullia integration)'
 	} >&2
 }
 
@@ -21,6 +22,30 @@ function evaluate {
 	nix eval --no-write-lock-file --json \
 		${CICERO_EVALUATOR_NIX_STACKTRACE:+--show-trace} \
 		"${CICERO_ACTION_SRC:-.}#ciceroActions" "$@"
+}
+
+function tullia {
+	if [[ "${CICERO_EVALUATOR_NIX_TULLIA:-1}" = 0 ]]; then return; fi
+
+	local job
+	job="$(< /dev/stdin)"
+
+	for attr in "${@}"; do
+		if [[ "$attr" != job ]]; then continue; fi
+
+		<<< "$job" jq --raw-output '
+			.job[].group[].task |
+			with_entries(select(
+				.value |
+				.driver == "podman" and (.config | has("image"))
+			)) |
+			keys_unsorted[]
+		' | while read -r task; do
+			# TODO support other systems?
+			nix run --no-write-lock-file \
+				"${CICERO_ACTION_SRC:-.}#task.x86_64-linux.\"$task\".oci.image.copyToRegistry"
+		done
+	done
 }
 
 case "${1:-}" in
@@ -42,22 +67,24 @@ eval)
 			--argstr attrs "${*}"
 	)"
 
-	evaluate --apply "$(
-		cat <<-EOF
-			actions:
-			let
-			  inherit (${vars}) attrs id inputs name;
-			  nonNullAttr = k: v: if v == null then {} else { \${k} = v; };
-			  action = actions.\${name} (
-			    nonNullAttr "id" id //
-			    nonNullAttr "inputs" inputs
-			  );
-			in
-			  __listToAttrs (map
-			    (name: { inherit name; value = action.\${name}; })
-			    (__filter (name: __elem name attrs) (__attrNames action)))
-		EOF
-	)"
+	#shellcheck disable=SC2016
+	result="$(evaluate --apply '
+		actions:
+		let
+		  inherit ('"$vars"') attrs id inputs name;
+		  nonNullAttr = k: v: if v == null then {} else { ${k} = v; };
+		  action = actions.${name} (
+		    nonNullAttr "id" id //
+		    nonNullAttr "inputs" inputs
+		  );
+		in
+		  __listToAttrs (map
+		    (name: { inherit name; value = action.${name}; })
+		    (__filter (name: __elem name attrs) (__attrNames action)))
+	')"
+	echo -n "$result"
+
+	echo -n "$result" | tullia "$@" >&2
 	;;
 '')
 	echo >&2 'No command given'
