input-output-hk/cicero

commit f2af280f93cc6f4650028503c9ba7e4d1a8bc418

make post-build-hook more robust

flake.nix
@@ -76,8 +76,10 @@
               #! /bin/dash
               set -euf
               export IFS=' '
-              echo 'Uploading to cache: '"$OUT_PATHS"
-              exec nix copy --to 'http://127.0.0.1:7745?compression=none' $OUT_PATHS
+              if [[ -n "$OUT_PATHS" ]]; then
+                echo 'Uploading to cache: '"$OUT_PATHS"
+                exec nix copy --to 'http://127.0.0.1:7745?compression=none' $OUT_PATHS
+              fi
             '';
 
             filter = ''

jobs/cicero.cue
@@ -190,8 +190,10 @@ if #env == "prod" {
 										"#! /bin/dash\\n" +
 										"set -euf\\n" +
 										"export IFS=\\\" \\\"\\n" +
-										"echo \\\"Uploading to cache: $OUT_PATHS\\\"\\n" +
-										"exec nix copy --to \\\"http://spongix.service.consul:7745?compression=none\\\" $OUT_PATHS"
+										"if [[ -n \\\"$OUT_PATHS\\\" ]]; then\\n" +
+										"\\techo \\\"Uploading to cache: $OUT_PATHS\\\"\\n" +
+										"\\texec nix copy --to \\\"http://spongix.service.consul:7745?compression=none\\\" $OUT_PATHS\\n" +
+										"fi"
 									),
 								}]
 							) end

nixos/configs/vm.nix
@@ -42,8 +42,10 @@
       post-build-hook = pkgs.writers.writeDash "post-build-hook" ''
         set -euf
         export IFS=' '
-        echo 'Uploading to cache: '"$OUT_PATHS"
-        exec nix copy --to 'http://127.0.0.1:7745?compression=none' $OUT_PATHS
+        if [[ -n "$OUT_PATHS" ]]; then
+          echo 'Uploading to cache: '"$OUT_PATHS"
+          exec nix copy --to 'http://127.0.0.1:7745?compression=none' $OUT_PATHS
+        fi
       '';
     in ''
       netrc-file = /etc/nix/netrc

commit d49ed7aae98b0a9095eda1bcbfc51a83aface503

fix post-build-hook
broken in f2af280

flake.nix
@@ -73,7 +73,7 @@
           treefmt-cue = final.callPackage pkgs/treefmt-cue.nix {};
           dev-cicero-transformer = let
             post-build-hook = ''
-              #! /bin/dash
+              #! /bin/bash
               set -euf
               export IFS=' '
               if [[ -n "$OUT_PATHS" ]]; then
@@ -102,7 +102,7 @@
                   perms: "544",
                   data: env.postBuildHook,
                 }] |
-                .config.packages |= . + ["github:NixOS/nixpkgs/${nixpkgs.rev}#dash"]
+                .config.packages |= . + ["github:NixOS/nixpkgs/${nixpkgs.rev}#bash"]
               ) end
             '';
           in

jobs/cicero.cue
@@ -7,7 +7,7 @@ let commonTransformers = [{
 	destination: "local/transformer.sh"
 	perms:       "544"
 	data: """
-		#! /bin/dash
+		#! /bin/bash
 		/bin/jq '
 			.job[]?.datacenters |= . + ["dc1"] |
 			.job[]?.group[]?.restart.attempts = 0 |
@@ -59,7 +59,7 @@ job: cicero: group: {
 			config: packages: [
 				#ciceroFlake,
 				// for transformers
-				"github:NixOS/nixpkgs/\(nixpkgsRev)#dash",
+				"github:NixOS/nixpkgs/\(nixpkgsRev)#bash",
 				"github:NixOS/nixpkgs/\(nixpkgsRev)#jq",
 			]
 		}
@@ -166,7 +166,7 @@ if #env == "prod" {
 					destination: "local/transformer-prod.sh"
 					perms:       "544"
 					data:        """
-						#! /bin/dash
+						#! /bin/bash
 						/bin/jq '
 							.job[]?.datacenters |= . + ["eu-central-1", "us-east-2"] |
 							.job[]?.group[]?.task[]? |= if .config?.nixos then . else (
@@ -182,12 +182,12 @@ if #env == "prod" {
 										.NIX_CONFIG
 									),
 								} |
-								.config.packages |= . + ["github:NixOS/nixpkgs/\(nixpkgsRev)#dash"] |
+								.config.packages |= . + ["github:NixOS/nixpkgs/\(nixpkgsRev)#bash"] |
 								.template |= . + [{
 									destination: "local/post-build-hook",
 									perms: "544",
 									data: (
-										"#! /bin/dash\\n" +
+										"#! /bin/bash\\n" +
 										"set -euf\\n" +
 										"export IFS=\\\" \\\"\\n" +
 										"if [[ -n \\\"$OUT_PATHS\\\" ]]; then\\n" +

nixos/configs/vm.nix
@@ -39,7 +39,7 @@
     binaryCachePublicKeys = ["spongix:yNfB2+pMSmrjNyMRWob1oEs4ihPnVKPkECWiDxv1MNI="];
     requireSignedBinaryCaches = false; # TODO remove once spongix signs with own key again
     extraOptions = let
-      post-build-hook = pkgs.writers.writeDash "post-build-hook" ''
+      post-build-hook = pkgs.writers.writeBash "post-build-hook" ''
         set -euf
         export IFS=' '
         if [[ -n "$OUT_PATHS" ]]; then