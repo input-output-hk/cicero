6c4a74f91ed04d4457f19f9879d7f39bdc6849ff
Robin Stumm <robin.stumm@iohk.io>
2022-05-13 16:18:16 +0200

    move MD5Sum to separate file

--- src/domain/types.go
+++ src/domain/types.go
@@ -309,17 +309,6 @@ type Invocation struct {
 
 type NomadEvent struct {
 	nomad.Event
-	Uid     MD5Sum
+	Uid     util.MD5Sum
 	Handled bool
-}
-
-type MD5Sum [16]byte
-
-func (self *MD5Sum) Scan(value interface{}) error {
-	if b, ok := value.([]byte); !ok {
-		return fmt.Errorf("Cannot scan %T into MD5Sum", value)
-	} else if copied := copy(self[:], b); copied != len(*self) {
-		return fmt.Errorf("Could only copy %d/%d bytes into MD5Sum", copied, len(*self))
-	}
-	return nil
 }
