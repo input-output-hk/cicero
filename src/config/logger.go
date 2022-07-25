b5fb1758f26f9cb104df55b04c98e0fc6121846f
Robin Stumm <robin.stumm@iohk.io>
2022-07-25 16:24:29 +0200

    remove unused function parameter

--- src/config/logger.go
+++ src/config/logger.go
@@ -41,7 +41,7 @@ type LoggerConfig struct {
 	MaxAge int
 }
 
-func buildLoggerConfig(level zerolog.Level) (*LoggerConfig, error) {
+func buildLoggerConfig() (*LoggerConfig, error) {
 	var conf LoggerConfig
 
 	if v, err := GetenvBool("CONSOLE_LOGGING_ENABLED"); err != nil {
@@ -97,7 +97,7 @@ func buildLoggerConfig(level zerolog.Level) (*LoggerConfig, error) {
 }
 
 func ConfigureLogger(level zerolog.Level) *zerolog.Logger {
-	config, err := buildLoggerConfig(level)
+	config, err := buildLoggerConfig()
 	if err != nil {
 		log.Fatal().Err(err).Msg("can't get logger config")
 		return nil
