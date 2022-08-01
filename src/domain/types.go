input-output-hk/cicero

commit 05c7c91841c3ab21f09da8430c8712080a52deb6

introduce invocations to DB

src/domain/types.go
@@ -212,10 +212,18 @@ type Action struct {
 }
 
 type Run struct {
-	NomadJobID uuid.UUID  `json:"nomad_job_id"`
-	ActionId   uuid.UUID  `json:"action_id"`
-	CreatedAt  time.Time  `json:"created_at"`
-	FinishedAt *time.Time `json:"finished_at"`
+	NomadJobID   uuid.UUID  `json:"nomad_job_id"`
+	InvocationId uuid.UUID  `json:"invocation_id"`
+	CreatedAt    time.Time  `json:"created_at"`
+	FinishedAt   *time.Time `json:"finished_at"`
+}
+
+type Invocation struct {
+	Id         uuid.UUID `json:"id"`
+	ActionId   uuid.UUID `json:"action_id"`
+	CreatedAt  time.Time `json:"created_at"`
+	EvalStdout *string   `json:"eval_stdout"`
+	EvalStderr *string   `json:"eval_stderr"`
 }
 
 type NomadEvent struct {

src/infrastructure/persistence/run.go
@@ -2,11 +2,9 @@ package persistence
 
 import (
 	"context"
-	"strconv"
 
 	"github.com/georgysavva/scany/pgxscan"
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v4"
 
 	"github.com/input-output-hk/cicero/src/config"
 	"github.com/input-output-hk/cicero/src/domain"
@@ -37,11 +35,21 @@ func (a *runRepository) GetByNomadJobIdWithLock(id uuid.UUID, lock string) (run 
 	)
 }
 
+func (a *runRepository) GetByInvocationId(invocationId uuid.UUID) (run domain.Run, err error) {
+	return run, pgxscan.Get(
+		context.Background(), a.DB, &run,
+		`SELECT * FROM run WHERE invocation_id = $1 `,
+		invocationId,
+	)
+}
+
 func (a *runRepository) GetByActionId(id uuid.UUID, page *repository.Page) ([]*domain.Run, error) {
 	runs := make([]*domain.Run, page.Limit)
 	return runs, fetchPage(
 		a.DB, page, &runs,
-		`*`, `run WHERE action_id = $1`, `created_at DESC`,
+		`run.*`,
+		`run JOIN invocation i ON i.id = run.invocation_id AND i.action_id = $1`,
+		`created_at DESC`,
 		id,
 	)
 }
@@ -49,35 +57,14 @@ func (a *runRepository) GetByActionId(id uuid.UUID, page *repository.Page) ([]*d
 func (a *runRepository) GetLatestByActionId(id uuid.UUID) (run domain.Run, err error) {
 	return run, pgxscan.Get(
 		context.Background(), a.DB, &run,
-		`SELECT DISTINCT ON (action_id) * FROM run WHERE action_id = $1 ORDER BY action_id, created_at DESC`,
+		`SELECT DISTINCT ON (invocation.action_id) run.*
+		FROM run
+		JOIN invocation ON
+			invocation.id = invocation_id AND
+			invocation.action_id = $1
+		ORDER BY invocation.action_id, run.created_at DESC`,
 		id,
 	)
-}
-
-func (a *runRepository) GetInputFactIdsByNomadJobId(id uuid.UUID) (inputFactIds repository.RunInputFactIds, err error) {
-	runInputs := []struct {
-		InputName string    `json:"input_name"`
-		FactId    uuid.UUID `json:"fact_id"`
-	}{}
-
-	err = pgxscan.Select(
-		context.Background(), a.DB, &runInputs,
-		`SELECT input_name, fact_id FROM run_inputs WHERE run_id = $1`,
-		id,
-	)
-	if err != nil {
-		return
-	}
-
-	inputFactIds = map[string][]uuid.UUID{}
-	for _, runInput := range runInputs {
-		if _, exists := inputFactIds[runInput.InputName]; !exists {
-			inputFactIds[runInput.InputName] = []uuid.UUID{}
-		}
-		inputFactIds[runInput.InputName] = append(inputFactIds[runInput.InputName], runInput.FactId)
-	}
-
-	return
 }
 
 func (a *runRepository) GetAll(page *repository.Page) ([]*domain.Run, error) {
@@ -88,106 +75,12 @@ func (a *runRepository) GetAll(page *repository.Page) ([]*domain.Run, error) {
 	)
 }
 
-func (a *runRepository) GetByInputFactIds(factIds []*uuid.UUID, recursive bool, page *repository.Page) ([]*domain.Run, error) {
-	joins := ``
-	for i := range factIds {
-		iStr := strconv.Itoa(i + 1)
-		joins += ` JOIN run_inputs AS run_inputs_` + iStr + ` ON
-			run_inputs_` + iStr + `.run_id = run.nomad_job_id AND
-			run_inputs_` + iStr + `.fact_id = $` + iStr
-	}
-
-	args := make([]interface{}, len(factIds))
-	for i, factId := range factIds {
-		args[i] = factId
-	}
-
-	var from string
-	if recursive {
-		from = `(
-			WITH RECURSIVE runs AS (
-				SELECT run.*
-				FROM run
-				` + joins + `
-
-				UNION
-
-				SELECT run.*
-				FROM run
-				JOIN runs ON
-					EXISTS (
-						SELECT NULL
-						FROM run_inputs
-						JOIN fact ON
-							fact.id = run_inputs.fact_id AND
-							fact.run_id = runs.nomad_job_id
-						WHERE run_inputs.run_id = run.nomad_job_id
-					)
-			) SELECT * FROM runs
-		) AS run`
-	} else {
-		from = `run ` + joins
-	}
-
-	runs := make([]*domain.Run, page.Limit)
-	return runs, fetchPage(
-		a.DB, page, &runs,
-		`run.*`, from, `created_at`,
-		args...,
-	)
-}
-
-func (a *runRepository) Save(run *domain.Run, inputs map[string]interface{}) error {
-	ctx := context.Background()
-
-	if err := a.DB.BeginFunc(ctx, func(tx pgx.Tx) error {
-		if err := tx.QueryRow(
-			ctx,
-			`INSERT INTO run (action_id) VALUES ($1) RETURNING nomad_job_id, created_at`,
-			run.ActionId,
-		).Scan(&run.NomadJobID, &run.CreatedAt); err != nil {
-			return err
-		}
-
-		if len(inputs) > 0 {
-			sql := `INSERT INTO run_inputs (run_id, input_name, fact_id) VALUES`
-			args := []interface{}{}
-
-			i := 1
-			for name, factOrFacts := range inputs {
-				addRow := func(factId uuid.UUID) {
-					if i > 1 {
-						sql += `, `
-					}
-
-					sql += `($` + strconv.Itoa(i) + `, $` + strconv.Itoa(i+1) + `, $` + strconv.Itoa(i+2) + `)`
-					args = append(args, run.NomadJobID, name, factId)
-					i += 3
-				}
-
-				switch factOrFactsTyped := factOrFacts.(type) {
-				case *domain.Fact:
-					addRow(factOrFactsTyped.ID)
-				case []*domain.Fact:
-					for _, fact := range factOrFactsTyped {
-						addRow(fact.ID)
-					}
-				default:
-					panic("inputs must be pointer to a Fact or array")
-				}
-			}
-
-			if _, err := tx.Exec(ctx, sql, args...); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	}); err != nil {
-		return err
-	}
-
-	return nil
+func (a *runRepository) Save(run *domain.Run) error {
+	return a.DB.QueryRow(
+		context.Background(),
+		`INSERT INTO run (invocation_id) VALUES ($1) RETURNING nomad_job_id, created_at`,
+		run.InvocationId,
+	).Scan(&run.NomadJobID, &run.CreatedAt)
 }
 
 func (a *runRepository) Update(run *domain.Run) (err error) {

src/application/service/run.go
@@ -34,13 +34,12 @@ type RunService interface {
 
 	GetByNomadJobId(uuid.UUID) (domain.Run, error)
 	GetByNomadJobIdWithLock(uuid.UUID, string) (domain.Run, error)
-	GetInputFactIdsByNomadJobId(uuid.UUID) (repository.RunInputFactIds, error)
+	GetByInvocationId(uuid.UUID) (domain.Run, error)
 	GetOutputByNomadJobId(uuid.UUID) (domain.RunOutput, error)
 	GetByActionId(uuid.UUID, *repository.Page) ([]*domain.Run, error)
 	GetLatestByActionId(uuid.UUID) (domain.Run, error)
 	GetAll(*repository.Page) ([]*domain.Run, error)
-	GetByInputFactIds([]*uuid.UUID, bool, *repository.Page) ([]*domain.Run, error)
-	Save(*domain.Run, map[string]interface{}, *domain.RunOutput) error
+	Save(*domain.Run, *domain.RunOutput) error
 	Update(*domain.Run) error
 	End(*domain.Run) error
 	Cancel(*domain.Run) error
@@ -108,10 +107,10 @@ func (self *runService) GetByNomadJobIdWithLock(id uuid.UUID, lock string) (run 
 	return
 }
 
-func (self *runService) GetInputFactIdsByNomadJobId(id uuid.UUID) (inputFactIds repository.RunInputFactIds, err error) {
-	self.logger.Trace().Str("nomad-job-id", id.String()).Msg("Getting Run input fact IDs by Nomad Job ID")
-	inputFactIds, err = self.runRepository.GetInputFactIdsByNomadJobId(id)
-	err = errors.WithMessagef(err, "Could not select Run input fact IDs by Nomad Job ID %q", id)
+func (self *runService) GetByInvocationId(invocationId uuid.UUID) (run domain.Run, err error) {
+	self.logger.Trace().Str("invocation-id", invocationId.String()).Msg("Getting Runs by input Fact IDs")
+	run, err = self.runRepository.GetByInvocationId(invocationId)
+	err = errors.WithMessagef(err, "Could not select Runs by Invocation ID %q", invocationId)
 	return
 }
 
@@ -143,17 +142,10 @@ func (self *runService) GetAll(page *repository.Page) (runs []*domain.Run, err e
 	return
 }
 
-func (self *runService) GetByInputFactIds(factIds []*uuid.UUID, recursive bool, page *repository.Page) (runs []*domain.Run, err error) {
-	self.logger.Trace().Int("offset", page.Offset).Int("limit", page.Limit).Interface("input-fact-ids", factIds).Bool("recursive", recursive).Msg("Getting Runs by input Fact IDs")
-	runs, err = self.runRepository.GetByInputFactIds(factIds, recursive, page)
-	err = errors.WithMessagef(err, "Could not select Runs by input fact IDs %q (recursively: %t) with offset %d and limit %d", factIds, recursive, page.Offset, page.Limit)
-	return
-}
-
-func (self *runService) Save(run *domain.Run, inputs map[string]interface{}, output *domain.RunOutput) error {
+func (self *runService) Save(run *domain.Run, output *domain.RunOutput) error {
 	self.logger.Trace().Msg("Saving new Run")
 	if err := self.db.BeginFunc(context.Background(), func(tx pgx.Tx) error {
-		if err := self.runRepository.WithQuerier(tx).Save(run, inputs); err != nil {
+		if err := self.runRepository.WithQuerier(tx).Save(run); err != nil {
 			return errors.WithMessagef(err, "Could not insert Run")
 		}
 		if err := self.runOutputRepository.WithQuerier(tx).Save(run.NomadJobID, output); err != nil {

src/domain/repository/run.go
@@ -12,34 +12,10 @@ type RunRepository interface {
 
 	GetByNomadJobId(uuid.UUID) (domain.Run, error)
 	GetByNomadJobIdWithLock(uuid.UUID, string) (domain.Run, error)
+	GetByInvocationId(uuid.UUID) (domain.Run, error)
 	GetByActionId(uuid.UUID, *Page) ([]*domain.Run, error)
 	GetLatestByActionId(uuid.UUID) (domain.Run, error)
-	GetInputFactIdsByNomadJobId(uuid.UUID) (RunInputFactIds, error)
 	GetAll(*Page) ([]*domain.Run, error)
-	GetByInputFactIds([]*uuid.UUID, bool, *Page) ([]*domain.Run, error)
-	Save(*domain.Run, map[string]interface{}) error
+	Save(*domain.Run) error
 	Update(*domain.Run) error
-}
-
-type RunInputFactIds map[string][]uuid.UUID
-
-func (self *RunInputFactIds) MapStringInterface(inputs map[string]domain.InputDefinition) (map[string]interface{}, error) {
-	result := map[string]interface{}{}
-	for name, factIds := range *self {
-		switch inputs[name].Select {
-		case domain.InputDefinitionSelectLatest:
-			if len(factIds) > 1 {
-				panic("This should never happen™")
-			}
-			result[name] = factIds[0]
-		case domain.InputDefinitionSelectAll:
-			result[name] = make([]*domain.Fact, len(factIds))
-			for i, factId := range factIds {
-				result[name].([]uuid.UUID)[i] = factId
-			}
-		default:
-			panic("unhandled case")
-		}
-	}
-	return result, nil
 }

db/migrations/20220425075544_create_invocations.sql
@@ -1,0 +1,63 @@
+-- migrate:up
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE invocation (
+	id uuid PRIMARY KEY DEFAULT public.gen_random_uuid(),
+	action_id uuid NOT NULL REFERENCES action (id) ON DELETE CASCADE,
+	created_at timestamp NOT NULL DEFAULT NOW(),
+
+	-- these are nullable because they are only interesting on error
+	eval_stdout text,
+	eval_stderr text,
+
+	-- Just a help column for this migration.
+	-- This is NOT part of the actual schema!
+	run_id uuid NOT NULL REFERENCES run (nomad_job_id)
+);
+
+-- Insert an invocation for every run.
+
+INSERT INTO invocation (action_id, created_at, eval_stdout, eval_stderr, run_id)
+SELECT action_id, created_at, '', '', nomad_job_id
+FROM run;
+
+-- Make every run point to its invocation.
+
+ALTER TABLE run
+ADD invocation_id uuid REFERENCES invocation (id) ON DELETE CASCADE;
+
+UPDATE run
+SET invocation_id = (
+	SELECT id
+	FROM invocation
+	WHERE invocation.run_id = run.nomad_job_id
+);
+
+-- We no longer need the help column.
+ALTER TABLE invocation
+DROP run_id;
+
+ALTER TABLE run
+DROP action_id;
+
+-- Change run_inputs to invocation_inputs.
+
+ALTER TABLE run_inputs
+RENAME TO invocation_inputs;
+
+ALTER TABLE invocation_inputs
+ADD invocation_id uuid REFERENCES invocation (id) ON DELETE CASCADE;
+
+UPDATE invocation_inputs
+SET invocation_id = (
+	SELECT invocation_id
+	FROM run
+	WHERE run.nomad_job_id = run_id
+);
+
+ALTER TABLE invocation_inputs
+ALTER invocation_id SET NOT NULL;
+
+ALTER TABLE invocation_inputs
+DROP run_id;

src/application/component/web/main.go
@@ -28,6 +28,7 @@ import (
 type Web struct {
 	Listen            string
 	Logger            zerolog.Logger
+	InvocationService service.InvocationService
 	RunService        service.RunService
 	ActionService     service.ActionService
 	FactService       service.FactService
@@ -149,6 +150,38 @@ func (self *Web) Start(ctx context.Context) error {
 			nil,
 			apidoc.BuildBodyRequest(apiActionPostBody{}), //TODO: move to domain
 			apidoc.BuildResponseSuccessfully(http.StatusNoContent, nil, "NoContent")),
+	); err != nil {
+		return err
+	}
+	if _, err := r.AddRoute(http.MethodGet,
+		"/api/invocation/{id}/inputs",
+		self.ApiInvocationIdInputsGet,
+		apidoc.BuildSwaggerDef(
+			apidoc.BuildSwaggerPathParams([]apidoc.PathParams{{Name: "id", Description: "id of an Invocation", Value: "UUID"}}),
+			nil,
+			apidoc.BuildResponseSuccessfully(http.StatusOK, domain.Run{}, "OK")),
+	); err != nil {
+		return err
+	}
+	if route, err := r.AddRoute(http.MethodGet,
+		"/api/invocation",
+		self.ApiInvocationByInputGet,
+		apidoc.BuildSwaggerDef(
+			nil,
+			nil,
+			apidoc.BuildResponseSuccessfully(http.StatusOK, []domain.Invocation{}, "OK")),
+	); err != nil {
+		return err
+	} else {
+		route.(*mux.Route).Queries("input", "")
+	}
+	if _, err := r.AddRoute(http.MethodGet,
+		"/api/invocation",
+		self.ApiInvocationGet,
+		apidoc.BuildSwaggerDef(
+			nil,
+			nil,
+			apidoc.BuildResponseSuccessfully(http.StatusOK, []domain.Invocation{}, "OK")),
 	); err != nil {
 		return err
 	}
@@ -490,6 +523,12 @@ func (self *Web) RunIdGet(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	invocation, err := self.InvocationService.GetById(run.InvocationId)
+	if err != nil {
+		self.ServerError(w, err)
+		return
+	}
+
 	allocs, err := self.NomadEventService.GetEventAllocByNomadJobId(id)
 	if err != nil {
 		self.NotFound(w, errors.WithMessagef(err, "Failed to find allocs for Nomad job %q", id))
@@ -502,7 +541,7 @@ func (self *Web) RunIdGet(w http.ResponseWriter, req *http.Request) {
 	}
 
 	inputs := map[string][]domain.Fact{}
-	if inputFactIds, err := self.RunService.GetInputFactIdsByNomadJobId(id); err != nil {
+	if inputFactIds, err := self.InvocationService.GetInputFactIdsById(run.InvocationId); err != nil {
 		self.ServerError(w, errors.WithMessage(err, "Failed to fetch input facts"))
 		return
 	} else {
@@ -585,7 +624,10 @@ func (self *Web) RunIdGet(w http.ResponseWriter, req *http.Request) {
 	}
 
 	if err := render("run/[id].html", w, map[string]interface{}{
-		"Run":        run,
+		"Run": struct {
+			domain.Run
+			ActionId uuid.UUID
+		}{run, invocation.ActionId},
 		"inputs":     inputs,
 		"output":     output,
 		"facts":      facts,
@@ -625,33 +667,43 @@ func (self *Web) RunGet(w http.ResponseWriter, req *http.Request) {
 	if page, err := getPage(req); err != nil {
 		self.BadRequest(w, err)
 		return
-	} else if runs, err := self.RunService.GetAll(page); err != nil {
+	} else if invocations, err := self.InvocationService.GetAll(page); err != nil {
 		self.ServerError(w, err)
 		return
 	} else {
-		type RunWrapper struct {
-			*domain.Run
-			Action *domain.Action
+		type Entry struct {
+			Run        *domain.Run
+			Invocation *domain.Invocation
+			Action     *domain.Action
 		}
 
-		runWrappers := make([]RunWrapper, len(runs))
-		for i, run := range runs {
-			if action, err := self.ActionService.GetById(run.ActionId); err != nil {
+		entries := make([]Entry, len(invocations))
+		for i, invocation := range invocations {
+			if action, err := self.ActionService.GetByInvocationId(invocation.Id); err != nil {
+				self.ServerError(w, err)
+				return
+			} else if run, err := self.RunService.GetByInvocationId(invocation.Id); err != nil && !pgxscan.NotFound(err) {
 				self.ServerError(w, err)
 				return
 			} else {
-				runWrappers[i] = RunWrapper{
-					Run:    run,
-					Action: &action,
+				var runPtr *domain.Run
+				if err == nil {
+					runPtr = &run
+				}
+
+				entries[i] = Entry{
+					Invocation: invocation,
+					Run:        runPtr,
+					Action:     &action,
 				}
 			}
 		}
 
 		if err := render("run/index.html", w, struct {
-			Runs []RunWrapper
+			Entries []Entry
 			*repository.Page
 		}{
-			Runs: runWrappers,
+			Entries: entries,
 			Page: page,
 		}); err != nil {
 			self.ServerError(w, err)
@@ -702,35 +754,84 @@ func (self *Web) ApiActionDefinitionSourceNameIdGet(w http.ResponseWriter, req *
 	}
 }
 
+func (self *Web) ApiInvocationGet(w http.ResponseWriter, req *http.Request) {
+	if page, err := getPage(req); err != nil {
+		self.ServerError(w, err)
+	} else if invocations, err := self.InvocationService.GetAll(page); err != nil {
+		self.ServerError(w, errors.WithMessage(err, "failed to fetch Invocations"))
+	} else {
+		self.json(w, invocations, http.StatusOK)
+	}
+}
+
 func (self *Web) ApiRunGet(w http.ResponseWriter, req *http.Request) {
 	if page, err := getPage(req); err != nil {
 		self.ServerError(w, err)
 	} else if runs, err := self.RunService.GetAll(page); err != nil {
-		self.ServerError(w, errors.WithMessage(err, "failed to fetch actions"))
+		self.ServerError(w, errors.WithMessage(err, "failed to fetch Runs"))
 	} else {
 		self.json(w, runs, http.StatusOK)
 	}
 }
 
-func (self *Web) ApiRunByInputGet(w http.ResponseWriter, req *http.Request) {
+func getByInputParams(req *http.Request) (bool, *bool, []*uuid.UUID, error) {
 	query := req.URL.Query()
+
 	_, recursive := query["recursive"]
+
+	var ok *bool
+	if okStr := query.Get("ok"); okStr != "" {
+		if ok_, err := strconv.ParseBool(okStr); err != nil {
+			return false, nil, nil, err
+		} else {
+			ok = &ok_
+		}
+	}
+
 	factIds := make([]*uuid.UUID, len(query["input"]))
 	for i, str := range query["input"] {
 		if id, err := uuid.Parse(str); err != nil {
-			self.ClientError(w, err)
-			return
+			return false, nil, nil, err
 		} else {
 			factIds[i] = &id
 		}
 	}
 
-	if page, err := getPage(req); err != nil {
+	return recursive, ok, factIds, nil
+}
+
+func (self *Web) ApiRunByInputGet(w http.ResponseWriter, req *http.Request) {
+	ok := true
+	if recursive, _, factIds, err := getByInputParams(req); err != nil {
+		self.ClientError(w, err)
+	} else if page, err := getPage(req); err != nil {
 		self.ServerError(w, err)
-	} else if runs, err := self.RunService.GetByInputFactIds(factIds, recursive, page); err != nil {
-		self.ServerError(w, errors.WithMessage(err, "failed to fetch actions"))
+	} else if invocations, err := self.InvocationService.GetByInputFactIds(factIds, recursive, &ok, page); err != nil {
+		self.ServerError(w, errors.WithMessage(err, "failed to fetch Invocations"))
 	} else {
+		runs := make([]*domain.Run, len(invocations))
+		for i, invocation := range invocations {
+			if run, err := self.RunService.GetByInvocationId(invocation.Id); err != nil {
+				self.ServerError(w, err)
+				return
+			} else {
+				runs[i] = &run
+			}
+		}
+
 		self.json(w, runs, http.StatusOK)
+	}
+}
+
+func (self *Web) ApiInvocationByInputGet(w http.ResponseWriter, req *http.Request) {
+	if recursive, ok, factIds, err := getByInputParams(req); err != nil {
+		self.ClientError(w, err)
+	} else if page, err := getPage(req); err != nil {
+		self.ServerError(w, err)
+	} else if invocations, err := self.InvocationService.GetByInputFactIds(factIds, recursive, ok, page); err != nil {
+		self.ServerError(w, errors.WithMessage(err, "failed to fetch Invocations"))
+	} else {
+		self.json(w, invocations, http.StatusOK)
 	}
 }
 
@@ -788,11 +889,27 @@ func (self *Web) ApiRunIdGet(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (self *Web) ApiInvocationIdInputsGet(w http.ResponseWriter, req *http.Request) {
+	if id, err := uuid.Parse(mux.Vars(req)["id"]); err != nil {
+		self.ClientError(w, err)
+	} else if inputs, err := self.InvocationService.GetInputFactIdsById(id); err != nil {
+		self.NotFound(w, errors.WithMessage(err, "Could not get Invocation's inputs"))
+	} else if action, err := self.ActionService.GetByInvocationId(id); err != nil {
+		self.ServerError(w, err)
+	} else if result, err := inputs.MapStringInterface(action.Inputs); err != nil {
+		self.ServerError(w, err)
+	} else {
+		self.json(w, result, http.StatusOK)
+	}
+}
+
 func (self *Web) ApiRunIdInputsGet(w http.ResponseWriter, req *http.Request) {
 	if id, err := uuid.Parse(mux.Vars(req)["id"]); err != nil {
 		self.ClientError(w, err)
-	} else if inputs, err := self.RunService.GetInputFactIdsByNomadJobId(id); err != nil {
-		self.NotFound(w, errors.WithMessage(err, "Could not get Run inputs"))
+	} else if run, err := self.RunService.GetByNomadJobId(id); err != nil {
+		self.NotFound(w, err)
+	} else if inputs, err := self.InvocationService.GetInputFactIdsById(run.InvocationId); err != nil {
+		self.NotFound(w, errors.WithMessage(err, "Could not get Run's Invocation's inputs"))
 	} else if action, err := self.ActionService.GetByRunId(id); err != nil {
 		self.ServerError(w, err)
 	} else if result, err := inputs.MapStringInterface(action.Inputs); err != nil {

src/application/component/web/templates/run/index.html
@@ -15,7 +15,7 @@
 			</tr>
 		</thead>
 		<tbody>
-			{{range .Runs}}
+			{{range .Entries}}
 				<tr>
 					<td>
 						{{with .Action}}
@@ -24,31 +24,47 @@
 							</a>
 						{{end}}
 					</td>
-					<td>{{.CreatedAt}}</td>
-					<td>
-						{{if .FinishedAt}}
-							{{.FinishedAt}}
-						{{else}}
-							<form
-								method="POST"
-								action="/_dispatch/method/DELETE/run/{{.NomadJobID}}"
-							>
-								<button>Cancel</button>
-							</form>
+					{{with .Run}}
+						<td>{{.CreatedAt}}</td>
+						<td>
+							{{if .FinishedAt}}
+								{{.FinishedAt}}
+							{{else}}
+								<form
+									method="POST"
+									action="/_dispatch/method/DELETE/run/{{.NomadJobID}}"
+								>
+									<button>Cancel</button>
+								</form>
+							{{end}}
+						</td>
+						<td class="numerical">
+							{{if .FinishedAt}}
+								{{.FinishedAt.Sub .CreatedAt}}
+							{{else}}
+								{{timeNow.Sub .CreatedAt}}
+							{{end}}
+						</td>
+						<td>
+							<a href="/run/{{.NomadJobID}}">
+								{{.NomadJobID}}
+							</a>
+						</td>
+					{{else}}
+						{{with .Invocation}}
+							<td>{{.CreatedAt}}</td>
+							<td colspan="3">
+								Failed:
+								<pre>
+								{{.EvalStdout}}
+								{{.EvalStderr}}
+								</pre>
+								<a href="/invocation/{{.Id}}">
+									{{.Id}}
+								</a>
+							</td>
 						{{end}}
-					</td>
-					<td class="numerical">
-						{{if .FinishedAt}}
-							{{.FinishedAt.Sub .CreatedAt}}
-						{{else}}
-							{{timeNow.Sub .CreatedAt}}
-						{{end}}
-					</td>
-					<td>
-						<a href="/run/{{.NomadJobID}}">
-							{{.NomadJobID}}
-						</a>
-					</td>
+					{{end}}
 				</tr>
 			{{end}}
 		</tbody>

commit e55714acaef88c2afbcae863c889ae57624c19a5

Add action_id field back to Run JSON serialization.
05c7c91841c3ab21f09da8430c8712080a52deb6 broke API compatibility.

src/domain/types.go
@@ -57,6 +57,7 @@ type Invocation struct {
 type Run struct {
 	NomadJobID   uuid.UUID  `json:"nomad_job_id"`
 	InvocationId uuid.UUID  `json:"invocation_id"`
+	ActionId     uuid.UUID  `json:"action_id"`
 	CreatedAt    time.Time  `json:"created_at"`
 	FinishedAt   *time.Time `json:"finished_at"`
 	Status       RunStatus  `json:"status"`

src/infrastructure/persistence/run.go
@@ -29,7 +29,7 @@ func (a runRepository) GetByNomadJobId(id uuid.UUID) (*domain.Run, error) {
 func (a runRepository) GetByNomadJobIdWithLock(id uuid.UUID, lock string) (*domain.Run, error) {
 	run, err := get(
 		a.DB, &domain.Run{},
-		`SELECT * FROM run WHERE nomad_job_id = $1 `+lock,
+		`SELECT r.*, i.action_id FROM run r WHERE r.nomad_job_id = $1 JOIN invocation i ON i.id = r.invocation_id `+lock,
 		id,
 	)
 	if run == nil {