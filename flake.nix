9197d858b23dcb03e0c36bc2bba487ca484d58ce
Shea Levy <shea@shealevy.com>
2022-10-12 08:09:53 -0400

    Add vm-bigdisk local vm

--- flake.nix
+++ flake.nix
@@ -166,6 +166,15 @@
           ];
         };
 
+        vm-bigdisk = nixpkgs.lib.makeOverridable nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {inherit self; bigDisk = true;};
+          modules = [
+            nixos/configs/vm.nix
+            nixos-shell.nixosModules.nixos-shell
+          ];
+        };
+
         dev = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = {inherit self;};
