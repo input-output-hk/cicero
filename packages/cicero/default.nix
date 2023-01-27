ccd5e1d8a1a7cd3d42355b8ba6b6316a4205c090
Robin Stumm <robin.stumm@iohk.io>
2023-01-27 17:27:40 +0100

    simplify schemathesis test setup

--- packages/cicero/default.nix
+++ packages/cicero/default.nix
@@ -47,50 +47,37 @@
               ifSystem isLinux {schemathesis = schemathesisTest;};
         };
 
-        # We cannot use pkgs.nixosTest because
-        # it gives us no way to inject specialArgs
-        # and _module.args lead to infinite recursion with self.
-        schemathesisTest =
-          (import "${inputs.nixpkgs}/nixos/lib/testing-python.nix" {
-            inherit system pkgs;
-            extraConfigurations = [
-              ({lib, ...}: {
-                nixpkgs = {
-                  inherit pkgs;
-                  overlays = lib.mkForce [
-                    (_: _: {cicero = package;})
-                  ];
-                };
-              })
+        schemathesisTest = pkgs.nixosTest {
+          name = "schemathesis";
+          nodes.main = {pkgs, ...}: {
+            imports = [config.flake.nixosModules.default];
+
+            nixpkgs.overlays = lib.mkForce [
+              (_: _: {cicero = package;})
             ];
-          })
-          .makeTest {
-            name = "schemathesis";
-            nodes.main = {pkgs, ...}: {
-              imports = [config.flake.nixosModules.default];
 
-              services = {
-                cicero = {
-                  enable = true;
-                  args = "web";
-                };
-
-                # The derivation implementing this accesses /var/log/{b,w}tmp at build time
-                # (should probably be fixed upstream).
-                # Without sandboxing, as is the case in an unprivileged container,
-                # this will fail due to file permissions not being open for the nix build user.
-                logrotate.checkConfig = false;
+            services = {
+              cicero = {
+                enable = true;
+                args = "web";
               };
 
-              environment.systemPackages = [(getSystem system).packages.schemathesis];
-
-              systemd.services.postgresql.serviceConfig.TimeoutStartSec = 300;
+              # The derivation implementing this accesses /var/log/{b,w}tmp at build time
+              # (should probably be fixed upstream).
+              # Without sandboxing, as is the case in an unprivileged container,
+              # this will fail due to file permissions not being open for the nix build user.
+              logrotate.checkConfig = false;
             };
-            testScript = ''
-              main.wait_for_unit("cicero")
-              main.succeed("schemathesis run http://127.0.0.1:8080/documentation/cicero.json --validate-schema=false --hypothesis-suppress-health-check=too_slow")
-            '';
+
+            environment.systemPackages = [(getSystem system).packages.schemathesis];
+
+            systemd.services.postgresql.serviceConfig.TimeoutStartSec = 300;
           };
+          testScript = ''
+            main.wait_for_unit("cicero")
+            main.succeed("schemathesis run http://127.0.0.1:8080/documentation/cicero.json --validate-schema=false --hypothesis-suppress-health-check=too_slow")
+          '';
+        };
       in
         package;
     };
