9d403108ac849bee56ea20d5995189acbf4992e6
Robin Stumm <robin.stumm@iohk.io>
2023-01-27 14:49:29 +0100

    format

--- packages/cicero/handbook.nix
+++ packages/cicero/handbook.nix
@@ -1,15 +1,16 @@
 {inputs, ...}: {
   perSystem = {pkgs, ...}: {
-    packages.cicero-handbook = pkgs.runCommand "cicero-handbook" {
-      buildInputs = with pkgs; [
-        mdbook
-        mdbook-mermaid
-      ];
-    } ''
-      mdbook build --dest-dir $out ${inputs.inclusive.lib.inclusive ../.. [
-        ../../book.toml
-        ../../handbook
-      ]}
-    '';
+    packages.cicero-handbook =
+      pkgs.runCommand "cicero-handbook" {
+        buildInputs = with pkgs; [
+          mdbook
+          mdbook-mermaid
+        ];
+      } ''
+        mdbook build --dest-dir $out ${inputs.inclusive.lib.inclusive ../.. [
+          ../../book.toml
+          ../../handbook
+        ]}
+      '';
   };
 }
